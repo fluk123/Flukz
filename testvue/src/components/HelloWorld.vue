8461b55ca2da42eda8159f5c84461f4b0ca040aa
fluk123 <60765814+fluk123@users.noreply.github.com>
2020-02-07 11:42:37 +0700

    testvue

--- testvue/src/components/HelloWorld.vue
+++ testvue/src/components/HelloWorld.vue
@@ -31,12 +31,70 @@
 </template>
 
 <script>
+import * as firebase from 'firebase'
+
+var firebaseConfig = {
+    apiKey: 'AIzaSyAl0ayME29LLCfyhyB1o2fAoMriLiedCJY',
+    authDomain: 'testvue-f686e.firebaseapp.com',
+    databaseURL: 'https://testvue-f686e.firebaseio.com',
+    projectId: 'testvue-f686e',
+    storageBucket: 'testvue-f686e.appspot.com',
+    messagingSenderId: '490102155967',
+    appId: '1:490102155967:web:782c8a973d3f0482c4340a',
+    measurementId: 'G-XZW1P1XTX8'
+  }
+  firebase.initializeApp(config)
+
+  var database = firebase.database()
+  var contactRef = database.ref('/contacts')
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+  data(){
+    return{
+        contacts:{},
+        name: '',
+        lastname: '',
+        updateLastName: '',
+        updateName: '',
+        updateKey: ''
+    }
+  },
+  methods: {
+    insertToContact(name,lastname){
+      let data = {
+      name: name,
+      lastname: lastname
+    }
+    contactRef.push(data)
+    this.name = ''
+    this.lastname = ''
+    
+  },
+   setUpdateContact (key,contact){
+     this.updateKey = key
+     this.updateLastName = contact.lastname
+     this.updateName = contact,name
+   },
+   updateContact(name ,lastname){
+     contactRef.child(this.updateKey).update({
+       name: name,
+       lastname: lastname
+     })
+     this.updateKey = ''
+     this.updateName = ''
+     this.updateLastName = ''
+   },
+   deleteContact(key){
+  contactRef.child(key).remove()
   }
+},
+mounted(){
+  contactRef.on('value',(snapshot) => {
+    this.contacts = snapshot.val()
+  })
 }
+}
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
